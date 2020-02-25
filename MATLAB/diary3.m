968707c28e80678c5cf14ccce90baaac3cc3c0e4
Max <55091443+azmuths602@users.noreply.github.com>
2020-02-25 15:59:31 +0200

    Add files via upload

---
+++ MATLAB/diary3.m
@@ -0,0 +1,39 @@
+%-- 02/25/2020 02:41:52 PM --%
+diary on
+y = A0+A*sin(2*pi*f*(t-delay))
+A0=0
+y = A0+A*sin(2*pi*f*(t-delay))
+A=0
+y = A0+A*sin(2*pi*f*(t-delay))
+A=1.5
+T=(3.5-1.5)/2
+f=1/T
+y = A0+A*sin(2*pi*f*(t-delay))
+t=[1.5:0.5:3.5];
+y = A0+A*sin(2*pi*f*(t-delay))
+edit
+lab3_demo
+lab3_demo.m
+lab3_demo
+lab3_demo.m
+lab3_demo
+zeros(2.4)
+zeros (2.4)
+zeros(2.4)
+zeros(2,4)
+A= zeros (2,4)
+size(A)
+format compact
+rand
+rand(2,4)
+lab3_demo
+t1 = [0 1 2 3];
+lab3_demo
+lab3_demo(t)
+lab3_demo(4)
+lab3_demo(0.7)
+lab3_demo(t)
+lab3_demo(0)
+lab3_demo
+lab3_demo(t)
+diary off
